868c790a419edf94839bbe5ff09b5e84beee95a9
fray23 <mars6358@gmail.com>
2023-06-16 16:51:31 +0600

    fix

--- restore_config.py
+++ restore_config.py
@@ -1,10 +1,10 @@
 import os
 import shutil
 
-def copy_config_files():
+def copy_config_files(config_dir, dest_dir):
     current_dir = os.getcwd()
-    config_dir = os.path.join(current_dir, '.config')
-    dest_dir = os.path.expanduser('~/.config')
+    config_dir = os.path.join(current_dir, config_dir)
+    dest_dir = os.path.expanduser(dest_dir)
 
     for root, dirs, files in os.walk(config_dir):
         rel_dir = os.path.relpath(root, config_dir)
@@ -18,4 +18,10 @@ def copy_config_files():
             dest_path = os.path.join(dest_root, file)
             shutil.copy2(src_path, dest_path)
 
-copy_config_files()
+file_list = [
+    ('.config', '~/.config'),
+    ('home', '~/'),
+]
+
+for config_dir, dest_dir in file_list:
+    copy_config_files(config_dir, dest_dir)
---
+++ save_config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp -R ~/.config/kitty .config/
+cp -R ~/.config/nvim .config/
+cp ~/.tmux.conf home/
+cp ~/.bashrc home/
