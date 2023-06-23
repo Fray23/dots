26f4d9d97707df94ac1de3841b8cde3c2e3403af
fray23 <mars6358@gmail.com>
2023-06-23 22:13:31 +0600

    update save_config.sh

--- save_config.sh
+++ save_config.sh
@@ -3,3 +3,5 @@ cp -R ~/.config/kitty .config/
 cp -R ~/.config/nvim .config/
 cp ~/.tmux.conf home/
 cp ~/.bashrc home/
+cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml .config/xfce4/xfconf/xfce-perchannel-xml/
+cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/keyboard-layout.xml .config/xfce4/xfconf/xfce-perchannel-xml/
