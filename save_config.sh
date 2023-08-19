62532c2ec3a93610f15014bd8b1ac66274e04bcf
fray23 <mars6358@gmail.com>
2023-08-19 19:49:20 +0600

    change

--- save_config.sh
+++ save_config.sh
@@ -6,3 +6,4 @@ cp ~/.bashrc home/
 cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml .config/xfce4/xfconf/xfce-perchannel-xml/
 cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/keyboard-layout.xml .config/xfce4/xfconf/xfce-perchannel-xml/
 cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml .config/xfce4/xfconf/xfce-perchannel-xml/
+cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml .config/xfce4/xfconf/xfce-perchannel-xml/
