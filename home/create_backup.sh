57aa10224cf588d09354c6f8ee4e8bc917df397d
mars6358@gmail.com <fray23>
2023-10-17 00:53:28 +0600

    update create_backup.sh scripts

--- home/create_backup.sh
+++ home/create_backup.sh
@@ -2,3 +2,5 @@
 
 backup_name=$(date +"%Y-%m-%d_%H-%M-%S")"_backup.tar.gz"
 tar cfz "/home/fs/backups/$backup_name" /home/fs/Desktop/work /home/fs/Desktop/a79 /home/fs/Desktop/qute /home/fs/.ssh /home/fs/.config/chromium
+tar cfz "/home/fs/backups/Pictures.tar.gz" /home/fs/Pictures
+tar cfz "/home/fs/backups/Documents.tar.gz" /home/fs/Documents
