c8f97fd7af919ca0d8383ffcc42159ff61734015
Marat <mars6358@gmail.com>
2024-01-07 03:12:36 +0600

    update

--- home/bin/scale.py
+++ home/bin/scale.py
@@ -20,9 +20,10 @@ if sys.argv[1] == '-s':
     replace_line_in_file('/home/fs/bin/rofi', 'rofi -modi drun -show drun -show-icons -icon-theme Papirus -dpi 100', 'rofi -modi drun -show drun -show-icons -icon-theme Papirus -dpi 150')
     replace_line_in_file('/home/fs/.config/chromium-flags.conf', '--force-device-scale-factor=1', '--force-device-scale-factor=1.5')
     replace_line_in_file('/home/fs/.Xresources', 'Xcursor.size: 24', 'Xcursor.size: 32')
+    replace_line_in_file('/home/fs/bin/rofi', '# while read -r line; do export "$line"; done < /home/fs/scale_vars', 'while read -r line; do export "$line"; done < /home/fs/scale_vars')
 elif sys.argv[1] == '-d':
     replace_line_in_file('/home/fs/.config/kitty/kitty.conf', 'font_size 16', 'font_size 11')
     replace_line_in_file('/home/fs/bin/rofi', 'rofi -modi drun -show drun -show-icons -icon-theme Papirus -dpi 150', 'rofi -modi drun -show drun -show-icons -icon-theme Papirus -dpi 100')
     replace_line_in_file('/home/fs/.config/chromium-flags.conf', '--force-device-scale-factor=1.5', '--force-device-scale-factor=1')
     replace_line_in_file('/home/fs/.Xresources', 'Xcursor.size: 32', 'Xcursor.size: 24')
-
+    replace_line_in_file('/home/fs/bin/rofi', 'while read -r line; do export "$line"; done < /home/fs/scale_vars', '# while read -r line; do export "$line"; done < /home/fs/scale_vars')
--- home/create_backup.sh
+++ home/create_backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 backup_name=$(date +"%Y-%m-%d_%H-%M-%S")"_backup.tar.gz"
-tar cf "/home/fs/backups/$backup_name" /home/fs/Desktop/work /home/fs/Desktop/a79 /home/fs/Desktop/qute /home/fs/.ssh /home/fs/.config/chromium
+tar cf "/home/fs/backups/$backup_name" /home/fs/Desktop/work /home/fs/Desktop/a79 /home/fs/.ssh /home/fs/.config/chromium
 tar cf "/home/fs/backups/Pictures.tar" /home/fs/Pictures
 tar cf "/home/fs/backups/Documents.tar" /home/fs/Documents
